6e448242774ff6aa6e7b5cc79a949f77331cd6fc
Vimal Kumar <Vimal2540.vk@gmail.com>
2022-11-02 15:13:37 +0530

    created checkout page along with checkout items and caheckout cart functionality

--- src/component/cart-dropdown/cart-dropdown.jsx
+++ src/component/cart-dropdown/cart-dropdown.jsx
@@ -1,12 +1,28 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { CartContext } from "../contexts/cart.context";
+
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
+  const navigate = useNavigate();
+
+  const checkOutHandler = () => navigate("/checkout");
+
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
-      <Button children="GO TO CHECKOUT" />
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button children="GO TO CHECKOUT" onClick={checkOutHandler} />
     </div>
   );
 };
--- src/component/product-card/product-card.component.jsx
+++ src/component/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+
+import { CartContext } from "../contexts/cart.context";
+
 import Button from "../button/button.component";
 
 import "./product-card.styles.scss";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -12,7 +19,11 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="proice">{price}</span>
       </div>
-      <Button buttonType="inverted" children="ADD TO CART" />
+      <Button
+        buttonType="inverted"
+        children="ADD TO CART"
+        onClick={addProductToCart}
+      />
     </div>
   );
 };
